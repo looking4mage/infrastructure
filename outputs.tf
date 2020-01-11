96f5d9a2f817d6e6256db6cf90df2d9e07157680
Rafal Proszowski <paroxp@gmail.com>
2020-01-11 00:29:16 +0000

    Define few outputs files for easier interaction
    Instead of going to DigitalOcean for the kubeconfig, we can obtain it
    directly from the terraform.

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_endpoint" {
+  value       = digitalocean_kubernetes_cluster.cluster.endpoint
+  description = "The cluster's API endpoint."
+}
+
+output "cluster_config" {
+  value       = digitalocean_kubernetes_cluster.cluster.kube_config.0.raw_config
+  description = "The kubeconfig file used to interact with the cluster via kubectl."
+  sensitive   = true
+}
+
